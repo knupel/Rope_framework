8fdba4f9dd5424f0aa66763c2360c6839a676ec2
Stan le Punk <stanislasiii@gmail.com>
2021-04-04 12:05:40 +0200

    add shader gradient

--- Rope_framework/Rope_framework.pde
+++ Rope_framework/Rope_framework.pde
@@ -9,77 +9,21 @@
 
 
 
-
-import rope.core.R_Graphic;
-import rope.R_State.State;
-
-
-/**
-* Crope Palette exemple
-* Processing 3.5.4
-* Rope library 0.12.1.41
-* v 0.1.0
-* 2021-2021
-*/
-
-
-PShader shader;
-PGraphics pg;
-int px;
-int py;
-int sx;
-int sy;
-
 void setup() {
-  px = 30;
-  py = 30;
-  sx = width - 2 *px;
-  sy = height - 2 *py;
-	// only in P2D or P3D
-  size(400, 400, P2D);
-  pg = createGraphics(sx, sy, P2D);
-  shader = loadShader("shader/fx_color/gradient.glsl");
-
+  size(200,200,P2D);
 }
 
 
 void draw() {
-  background(0);
-  float arg = constrain(map(mouseX,0,width,0,1),0,1);
-  
-  // gradiant spectrum color
-  shader.set("min_hue",0.5);
-  shader.set("max_hue",1.0);
-
-  // gradient hue
-  shader.set("hue",arg);
-  shader.set("start_sat",0.7);
-  shader.set("min_sat",0.0);
-  shader.set("max_sat",1.0);
-
-  shader.set("start_bri",0.9);
-  shader.set("min_bri",0.0);
-  shader.set("max_bri",1.0);
-
-  // direction of the gradient
-  shader.set("vert_is",false);
-  // shader.set("mode",1);
-
-  // noStroke();
-  pg.shader(shader);
-  pg.beginDraw();
-  pg.rect(0,0,sx,sy,0);
-  // pg.beginShape();
-  // pg.vertex(0,0); // top left
-  // pg.vertex(sx,0); // top right
-  // pg.vertex(sx, sy); // bottom left
-  // pg.vertex(0, sy); // bottom right
-  // pg.endShape();
-  pg.endDraw();
-  image(pg,px,py);
-
-
+  background(r.ORANGE);
+  fill(r.BLOOD);
+  stroke(r.BLACK);
+  strokeWeight(2);
   ellipse(mouseX,mouseY, 50,50);
+  noFill();
+  stroke(r.WHITE);
+  rect(20,20, width -40, height -40);
+
 
 }
 
--- Rope_framework/data/shader/fx_color/gradient.glsl
+++ Rope_framework/data/shader/fx_color/gradient.glsl
@@ -1,7 +1,7 @@
 /**
 * Palette shader
 * 2021-2021
-* v 0.0.2
+* v 0.0.3
 * @author @stanlepunk
 * @see https://github.com/StanLepunK/Rope
 */
@@ -17,34 +17,21 @@ uniform vec2 range;
 uniform float min_hue = 0.0;
 uniform float max_hue = 1.0;
 
-uniform float start_sat = 0.0;
+uniform float start_sat = 0.5;
 uniform float min_sat = 0.0;
 uniform float max_sat = 1.0;
 
-uniform float start_bri = 0.0;
+uniform float start_bri = 0.5;
 uniform float min_bri = 0.0;
 uniform float max_bri = 1.0;
 uniform int mode = 0;
 uniform bool vert_is = false;
 
-// https://thebookofshaders.com/05/?lan=fr
-//  Function from Iñigo Quiles
-//  https://www.shadertoy.com/view/MsS3Wc
-vec3 rgb_to_hsb( in vec3 c ){
-	vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
-	vec4 p = mix(	vec4(c.bg, K.wz),
-            		vec4(c.gb, K.xy),
-              	step(c.b, c.g));
-	vec4 q = mix(	vec4(p.xyw, c.r),
-          			vec4(c.r, p.yzx),
-								step(p.x, c.r));
-	float d = q.x - min(q.w, q.y);
-	float e = 1.0e-10;
-	return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)),
-									d / (q.x + e),
-									q.x);
-}
-
+/**
+* @see https://thebookofshaders.com/05/?lan=fr
+* Function from Iñigo Quiles
+* @see https://www.shadertoy.com/view/MsS3Wc
+*/
 vec3 hsb_to_rgb(vec3 c){
 	vec3 rgb = clamp(abs(mod(c.x*6.0+vec3(0.0,4.0,2.0),
                              6.0)-3.0)-1.0,
@@ -74,15 +61,15 @@ void main(void) {
 	} else {
 		float sat = 1.0;
 		float bri = 1.0;
-		// if(val < 0.5) bri = map(val, 0, 0.5, 0, 1.0); // base ok
-		// if(val > 0.5) sat = map(val, 0.5, 1.0, 1.0, 0); // base ok
 
 		if(val < start_bri) bri = map(val, 
 																	0, start_bri,
-																	0, 1.0);		
+																	min_bri, max_bri);		
 		if(val > start_sat) sat = map(val,
 																	start_sat, 1.0, 
-																	1.0, 0);
+																	max_sat, min_sat);
+
+
 		color = hsb_to_rgb(vec3(hue,sat,bri)); // from white to color to black
 		
 	}
